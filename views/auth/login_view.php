8731607cbe6175c35e740352f86dd23c9088db7d
unknown <Fakhri>
2026-02-23 15:58:23 +0700

    songkosong

--- views/auth/login_view.php
+++
@@ -1,5 +0,0 @@
-<form method="POST" action="../controllers/login.php">
-    <input type="email" name="email">
-    <input type="password" name="password">
-    <button type="submit">Login</button>
-</form>
--- views/auth/register_view.php
+++
@@ -1,6 +0,0 @@
-<form method="POST" action="../controllers/login.php">
-    <input type="email" name="email">
-    <input type="password" name="password">
-    <input type="password" name="confirm">
-    <button type="submit">Login</button>
-</form>
