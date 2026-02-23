52c266d6ccee49595b25216421417bf6360d5f10
unknown <Fakhri>
2026-02-23 16:00:45 +0700

    memulai lembaran baru asek

---
+++ config/database.php
@@ -0,0 +1,2 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "task_management");
---
+++ controllers/register.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+
+require '../config/database.php';
+require '../services/auth_service.php';
+require '../helpers/helper.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $username = $_POST['username'] ?? '';
+    $email    = $_POST['email'] ?? '';
+    $password = $_POST['password'] ?? '';
+    $confirm  = $_POST['confirm'] ?? '';
+
+    $result = registerUser($username, $email, $password,  $confirm);
+
+    if ($result['success']) {
+        header("Location: login.php");
+        exit;
+    } else {
+        $error = $result['message'];
+    }
+}
